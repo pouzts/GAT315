164db8899829fba6fcefe91a4ea125d0034120ce
Peter Ouzts <81652748+pouzts@users.noreply.github.com>
2022-04-06 02:00:25 -0600

    6 April 2022 - Worked on Gravitation.
    Not completely sure if it is working.

--- Assets/Scripts/Forces/GravitationForce.cs
+++ Assets/Scripts/Forces/GravitationForce.cs
@@ -8,6 +8,22 @@ public class GravitationForce : Force
 
     public override void ApplyForce(List<Body> bodies)
     {
-        throw new System.NotImplementedException();
+        for (int i = 0; i < bodies.Count - 1; i++)
+        {
+            for (int j = i + 1; j < bodies.Count; j++)
+            {
+                Body bodyA = bodies[i];
+                Body bodyB = bodies[j];
+
+                // apply gravitational force
+                Vector2 direction = bodyA.position - bodyB.position;
+                float distanceSqr = Mathf.Max(direction.magnitude * direction.magnitude, 1); //d^2
+
+                float gravitationForce = gravitation.value * (bodyA.mass * bodyB.mass / distanceSqr); //G(m1*m2/d^2)
+
+                bodyA.ApplyForce(-direction.normalized * gravitationForce, Body.eForceMode.Force);
+                bodyB.ApplyForce(direction.normalized * gravitationForce, Body.eForceMode.Force);
+            }
+        }
     }
 }
